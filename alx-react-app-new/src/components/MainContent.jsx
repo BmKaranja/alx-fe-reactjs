1d1984922180adf3271cf4f8e5ffbc561d164c09
Brian Karanja <bmkaranja001@gmail.com>
2025-11-05 15:19:00 +0300

    Styled my react app

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 export function MainContent(){
     return(
-        <main>
-            <p style={{color:"yellow"}}>I love to visit <span style={{textDecoration:"underline"}}>New York</span>, Paris, and Tokyo.</p>
+        <main style={{backgroundColor:"whitesmoke"}}>
+            <p style={{color:"purple"}}>I love to visit <span style={{textDecoration:"underline"}}>New York</span>, Paris, and Tokyo.</p>
         </main>
     )
 }
