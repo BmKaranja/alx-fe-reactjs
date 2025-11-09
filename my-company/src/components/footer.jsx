a5f42f01c122cfe573049c64a7b02ef8d9ccaf05
Brian Karanja <bmkaranja001@gmail.com>
2025-11-09 11:15:00 +0300

    my website pages

---
+++ my-company/src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+      <p>
+        © 2024 Our Company. All rights reserved.
+      </p>
+      <p>
+        Contact us: <a href='maiito: bmkaranja0012gmail.com'>Bmkaranja001@gmail.com</a>
+      </p>
+    </div>
+  )
+}
+
+export default footer
