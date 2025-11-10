82284aef62ee38fa4c2680f378f99444fdf488bc
Brian Karanja <bmkaranja001@gmail.com>
2025-11-10 11:41:24 +0300

    routing

--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react"
+
 function Home() {
     return (
     <div style={{ padding: '20px' }}>
+        <nav>
+            <ul>
+                <Link to ="/">Home</Link>
+                <Link to ="/About">About</Link>
+            </ul>
+        </nav>
         <h1>Welcome to Our Company</h1>
         <p>We are dedicated to delivering excellence in all our services.</p>
     </div>
--- my-company/src/main.jsx
+++ my-company/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './src/App'
+import About from './src/components/About'
+import Contact from './src/components/Contact'
+import { createBrowserRouter , RouterProvider } from 'react-router'
 
+const router=createBrowserRouter([
+  {path:"/", element:<Home/>},
+  {path:"/About",element:<About/>},
+  {path:"/Contact",element:<Contact/>},
+  {path:"/Service",element:<Service/>}   
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
