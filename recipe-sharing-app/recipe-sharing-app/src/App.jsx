a371db137bada98a1b7b8fc4c5364cf09473a870
Brian Karanja <bmkaranja001@gmail.com>
2025-11-11 11:07:24 +0300

    zustand

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <RecipeList/>
+      <AddRecipeForm/>
+    </>
+  )
+}
+
+export default App
