aa85194523f07dff1b3b450d30f3caabf263c529
Brian Karanja <bmkaranja001@gmail.com>
2025-11-11 15:12:30 +0300

    zustand-4

--- recipe-sharing-app/TODO.md
+++ recipe-sharing-app/TODO.md
@@ -1,7 +1,10 @@
-# TODO List for Extending Zustand Store
+# TODO List for Extending Zustand Store and UI
 
 - [x] Extend Zustand store in `src/components/recipeStore.js`:
   - Add `searchTerm` state (initially empty string).
   - Add `filteredRecipes` state (initially same as `recipes`).
   - Add `setSearchTerm` action to update `searchTerm`.
   - Add `filterRecipes` action to compute `filteredRecipes` based on `searchTerm` (filter by title or ingredients, case insensitive).
+
+- [x] Modify RecipeList component to display filteredRecipes instead of recipes.
+- [x] Add a search bar to the UI for updating the search term.
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,27 @@
 import useRecipeStore from './recipeStore';
 import { Link } from 'react-router-dom';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const searchTerm = useRecipeStore(state => state.searchTerm);
+  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3><Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link></h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
-  export default RecipeList;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search recipes..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredRecipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3><Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link></h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
