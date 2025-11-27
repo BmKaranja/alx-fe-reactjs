5697c124e9d101f36342706874f6f5674f5441f7
Brian Karanja <bmkaranja001@gmail.com>
2025-11-27 16:53:33 +0300

    try 1

---
+++ tailwind-react-integration/tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserProfile() {
+  return (
+    <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto,m-20 rounded-lg hover:shadow-xl shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">
+      <img className="rounded-full w-36 h-36 hover:scale-100,transition-transform, duration-300, ease-in-out" src="https://via.placeholder.com/150" alt="User" />
+      <h1 className='text-xl hover:text-blue-600 text-blue-800 my-4 sm:text-lg md:text-base'>John Doe</h1>
+      <p className='text-gray-600 text-base sm:w-24,h-24 md w-36,h-36'>Developer at Example Co. Loves to write code and explore new technologies.</p>
+    </div>
+  )
+}
+
+export default UserProfile
