cee7d5411a404a0393f23f3ff2151d8823481c33
Brian Karanja <bmkaranja001@gmail.com>
2025-12-03 15:34:54 +0300

    #2

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import HomePage from './components/HomePage'
 import {
-  Routes, Route
+  Routes, Route, BrowserRouter as Router
 } from "react-router-dom";
 import RecipeDetail from './components/RecipeDetail';
 function App() {
@@ -10,11 +10,13 @@ function App() {
     <>
       <div>
       <HomePage/>
+      <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
         <Route path="/add-recipe" element={<AddRecipeForm />} />
       </Routes>
+      </Router>
       </div>
     </>
   )
